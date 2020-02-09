6f460bd1cfb1d3b952b46d3c851c0df92ac99aba
natysh1011 <natysh1011@gmail.com>
2020-02-09 15:00:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿//2.	כתוב תוכנית הקולטת מהמשתמש 10 מספרים שלמים והצג כמה מספרים שהוקלדו אי-זוגיים
+using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int d = 0;
+            for (int a = 0; 10 > a; a++)
+            {
+                int c = int.Parse(Console.ReadLine());
+
+                if (c % 2 == 0)
+                {
+
+                }
+                else
+                {
+                    d++;
+                }
+            }
+            Console.WriteLine(d);
+        }
+    }
+}
